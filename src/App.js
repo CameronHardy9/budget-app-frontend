93e2a40449187fd5a83ec94958db238cf84eb990
Cameron Hardy <camhardy99@gmail.com>
2021-12-31 11:37:13 -0600

    Removed unused component. Updated variable names. Added state update functions. Added props for handing state in child components. Updated useEffect dependancy array.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import apiHandler from './components/apiHandler';
+import apiHandler from './utils/apiHandler';
 import {useState, useEffect} from 'react';
-import DataBox from './components/DataBox';
 import {Routes, Route} from 'react-router-dom';
 import HomeMenu from './components/HomeMenu';
 import Purchases from './components/Purchases';
@@ -9,31 +8,31 @@ import Item from './components/Item';
 
 function App() {
 
-    const [budget, setBudget] = useState(undefined);
+    const [userObject, setUserObject] = useState(undefined);
 
     useEffect(() => {
        (async () => {
            const response = await apiHandler("GET");
-           setBudget(response.budget);
+           setUserObject(response);
        })();
-    },[budget]);
+    },[]);
 
-    async function clickHandler() {
-        const response = await apiHandler("GET");
-        console.log(response);
+    function updateUserObject(object) {
+        setUserObject(object);
     };
 
     return (
         <>
             <Routes>
-                <Route path="/" element={<HomeMenu />}>
-                    <Route path="budget" element={<Budget />} />
-                    <Route path="purchases" element={<Purchases />} >
-                        <Route path="item" element={<Item />} />
-                    </Route>
-                </Route>
+                <Route path="/" element={<HomeMenu loaded={!!userObject} />} />
+                <Route path="budget" element={<Budget userObject={userObject} />} />
+                <Route path="purchases" element={<Purchases userObject={userObject} />} />
+                {!!userObject && userObject.purchases.map((item) => {
+                    return(
+                        <Route key={item.uniqid} path={item.uniqid} element={<Item userObject={userObject} item={item} updateUserObject={updateUserObject} />} />
+                    )
+                })}
             </Routes>
-            <DataBox />
         </>
     );
 }
