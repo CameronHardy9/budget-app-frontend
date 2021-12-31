9af8a86f5bc8fdf597edede7696eb545bf1debc5
Cameron Hardy <camhardy99@gmail.com>
2021-12-31 11:39:23 -0600

    Added item rendering and function for deleting item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,27 @@
 import {Link} from 'react-router-dom';
+import apiHandler from '../utils/apiHandler';
 
-function Item() {
+function Item(props) {
     return(
-        <h1>Item</h1>
+        <div className="container">
+            <h1>Item</h1>
+            <div className='dataBox'>
+                <h2 className={``}>{props.item.store}</h2>
+                <h2 className={``}>${props.item.amount}</h2>
+                <h2 className={``}>{props.item.date}</h2>
+            </div>
+            <Link to=".." onClick={async() => {
+                const newDoc = await clickHandler(props.item.uniqid);
+                props.updateUserObject(newDoc);
+                }}>Delete Purchase</Link>
+            <Link to="../purchases">Back</Link>
+        </div>
     )
+}
+
+async function clickHandler(purchaseId) {
+    const newDoc = await apiHandler("DELETE", {uniqid: purchaseId});
+    return newDoc;
 }
 
 export default Item;
