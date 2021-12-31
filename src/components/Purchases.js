d87e5a9ea6cef9f1e09fa63332e4f71151fbb9b5
Cameron Hardy <camhardy99@gmail.com>
2021-12-31 11:39:52 -0600

    Added purchase list rendering. Added function for adding purchases.

--- src/components/Purchases.js
+++ src/components/Purchases.js
@@ -1,12 +1,47 @@
 import {Link} from 'react-router-dom';
+import apiHandler from '../utils/apiHandler';
+import {useState} from 'react';
 
-function Purchases() {
+function Purchases(props) {
+
+    const [purchases, setPurchases] = useState(props.userObject.purchases);
+
     return(
-        <>
+        <div className='container'>
             <h1>Purchases</h1>
-            <Link to="item">Item</Link>
-        </>
+            {purchases.map((item) => {
+                return (
+                    <Link to={`../${item.uniqid}`} key={item.uniqid}>
+                        <div className={`dataBox`}>
+                            <span>{item.store}</span>
+                            <span>${item.amount}</span>
+                            <span>{item.date}</span>
+                        </div>
+                    </Link>
+                )
+            })}
+            <button onClick={async () => {
+                const newDoc = await clickHandler();
+                setPurchases(newDoc);
+            }}>Add Purchase</button>
+            <Link to="..">Back</Link>
+        </div>
     )
+}
+
+async function clickHandler() {
+    const store = prompt("Store name?");
+    const amount = Number(prompt("Purchase amount?"));
+    const date = new Date().toLocaleDateString("en-US").replaceAll("/","-");;
+
+    const body = {
+        store: store,
+        amount: amount,
+        date: date
+    }
+
+    const newDoc = await apiHandler("PUT", body);
+    return newDoc.purchases;
 }
 
 export default Purchases;
