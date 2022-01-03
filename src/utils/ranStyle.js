ffe814869f420824c5bab1697c8f89e347fd5369
Cameron Hardy <camhardy99@gmail.com>
2022-01-03 14:03:38 -0600

    removed ranStyle

--- src/utils/ranStyle.js
+++
@@ -1,7 +0,0 @@
-function ranStyle() {
-    const styles = ["faded1", "faded2", "faded3", "faded4", "faded5"];
-    const rand = Math.round(Math.random() * 4);
-    return styles[rand];
-}
-
-export default ranStyle;
